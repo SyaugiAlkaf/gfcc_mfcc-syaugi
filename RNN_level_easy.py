3b91867320833eb4d52dcc0a56fc421e31b10446
Syaugi Alkaf <125639879+SyaugiAlkaf@users.noreply.github.com>
2023-09-19 11:41:02 +0700

    Create RNN_level_easy.py
    more lighter than the hard one since the dataset was too large

---
+++ RNN_level_easy.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dropout, Dense
+
+# Load the CSV data
+X = pd.read_csv('X.csv').values
+y = pd.read_csv('y.csv')['label'].values
+
+# Reshape X for RNN input
+X = X.reshape(X.shape[0], X.shape[1], 1)
+
+# Encode the labels
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+# Split the data into training and test/validation sets
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+
+# Build the simplified RNN model
+model = Sequential()
+model.add(LSTM(32, return_sequences=False, input_shape=(X_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(len(np.unique(y_encoded)), activation='softmax'))
+
+# Compile the model
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model with a smaller batch size
+model.fit(X_train, y_train, epochs=10, batch_size=16, validation_data=(X_test, y_test))
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f"Test Loss: {loss:.4f}")
+print(f"Test Accuracy: {accuracy:.4f}")
+
+# Predict a sample
+sample = X_test[0].reshape(1, X_test.shape[1], 1)
+predicted_label = label_encoder.inverse_transform(np.argmax(model.predict(sample), axis=-1))[0]
+print(f"Predicted Condition: {predicted_label}")
